8a3f50ab3d8488e9b83c2ec7c0c17dd48d256a7a
vunguyen-biglabs <vu.nguyen@big-labs.com>
2018-05-29 16:45:15 +0700

    Add html and css

--- src/App.js
+++ src/App.js
@@ -17,14 +17,85 @@ class App extends Component {
         };
 
         const web3 = new Web3(window.web3.currentProvider);
-
+        this.state.web3 = web3;
         this.state.ContractInstance = new web3.eth.Contract(abi, "0x4cA032A45c9EbAE15d9446FbA2DcD58b379E51a3");
 
         window.a = this.state
     }
 
 
-    componentDidMount() {
+    componentDidMount(){
+        this.updateState();
+        this.setupListeners();
+
+        setInterval(this.updateState.bind(this), 7e3)
+    }
+
+    updateState(){
+
+        // this.state.ContractInstance.methods.minimumBet().then(function(data) {
+        //     this.setState({minimumBet: parseFloat(this.state.web3.fromWei(data, 'ether'))});
+        // }).catch(function (error){
+        //     console.log(error);
+        // });
+        // this.state.ContractInstance.methods.totalBet((err, result) => {
+        //     if(result != null){
+        //         this.setState({
+        //             totalBet: parseFloat(this.state.web3.fromWei(result, 'ether'))
+        //         })
+        //     }
+        // });
+        // this.state.ContractInstance.methods.numberOfBets((err, result) => {
+        //     if(result != null){
+        //         this.setState({
+        //             numberOfBets: parseInt(result)
+        //         })
+        //     }
+        // });
+        // this.state.ContractInstance.methods.maxAmountOfBets((err, result) => {
+        //     if(result != null){
+        //         this.setState({
+        //             maxAmountOfBets: parseInt(result)
+        //         })
+        //     }
+        // })
+    }
+
+
+    // Listen for events and executes the voteNumber method
+    setupListeners(){
+        let liNodes = this.refs.numbers.querySelectorAll('li');
+        liNodes.forEach(number => {
+            number.addEventListener('click', event => {
+                event.target.className = 'number-selected';
+                this.voteNumber(parseInt(event.target.innerHTML), done => {
+
+                    // Remove the other number selected
+                    for(let i = 0; i < liNodes.length; i++){
+                        liNodes[i].className = ''
+                    }
+                })
+            })
+        })
+    }
+
+    voteNumber(number, cb){
+        let bet = this.refs['ether-bet'].value;
+
+        if(!bet) bet = 0.1;
+
+        if(parseFloat(bet) < this.state.minimumBet){
+            alert('You must bet more than the minimum');
+            cb()
+        } else {
+            this.state.ContractInstance.bet(number, {
+                gas: 300000,
+                from: this.state.web3.eth.accounts[0],
+                value: this.state.web3.toWei(bet, 'ether')
+            }, (err, result) => {
+                cb()
+            })
+        }
     }
 
     render() {
